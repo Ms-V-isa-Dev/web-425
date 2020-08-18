d1ba931e0bcb06f017e4b43fb2101df7a86eba28
Verlee Washington <VWashington@my365.bellevue.edu>
2020-08-17 21:28:34 -0500

    Adding assignment

---
+++ Week 6/gpa-calculator-app1/src/app/app.component.ts
@@ -0,0 +1,17 @@
+/**
+ * Title: Assignment 6.3
+ * Author: Verlee Washington
+ * Date: 08/17/2020
+ * Description: App components.ts
+ */
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+}
---
+++ Week 6/gpa-calculator-app1/src/app/home/home.component.ts
@@ -0,0 +1,22 @@
+/**
+ * Title: Assignment 6.3
+ * Author: Verlee Washington
+ * Date: 08/17/2020
+ * Description: Home component.ts
+ */
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
