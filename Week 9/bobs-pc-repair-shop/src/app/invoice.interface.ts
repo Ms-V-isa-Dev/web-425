47742f4295a591f57fad49451dc93b4000dd3233
Verlee Washington <VWashington@my365.bellevue.edu>
2020-09-17 20:42:41 -0500

    update

--- Week 9/bobs-pc-repair-shop/src/app/invoice.interface.ts
+++ Week 9/bobs-pc-repair-shop/src/app/invoice.interface.ts
@@ -7,6 +7,8 @@
 
 import { IRepairService } from './repair-service.interface';
 
+//When user submits a service repair form
+//This is how we enforce data
 export interface Invoice {
   lineItems: Array<IRepairService>;
   partsAmount: number;
